6d2033492a98ae60633741698486b802e12b5b20
mkielak <kielakowsky17@gmail.com>
2026-06-01 18:10:20 +0200

    feat(common): create common module and add tick event in it

--- backend/common/src/main/java/com/marketpulse/common/event/TickEvent.java
+++ backend/common/src/main/java/com/marketpulse/common/event/TickEvent.java
@@ -1,4 +1,12 @@
 package com.marketpulse.common.event;
 
-public record TickEvent() {
-}
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record TickEvent(
+        String symbol,
+        BigDecimal price,
+        BigDecimal quantity,
+        long tradeId,
+        Instant timestamp
+) {}
